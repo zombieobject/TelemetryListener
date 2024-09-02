838d6bf787775b72f8cb0c3e2e06b25ad3ba39b5
Ethan Mateja <ethan@passivelogic.com>
2024-09-02 11:45:23 -0700

    Project Shell

---
+++ TelemetryListener/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TelemetryListener",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TelemetryListener",
+            targets: ["TelemetryListener"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "TelemetryListener"),
+        .testTarget(
+            name: "TelemetryListenerTests",
+            dependencies: ["TelemetryListener"]),
+    ]
+)
